8779f60320f7b957216a632c8a1dae60c589d06a
sajan1789 <sajan99559@gmail.com>
2024-07-31 00:10:43 +0530

    deployment-errpr

--- my-app/src/app/contact/page.jsx
+++ my-app/src/app/contact/page.jsx
@@ -85,9 +85,9 @@ function SignupFormDemo() {
              disabled:cursor-not-allowed disabled:opacity-50
              dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
              group-hover/input:shadow-none transition duration-400"
-                            rows={5}
+                            
                             required
-                        > <BottomGradient /></textarea>
+                        > </textarea>
                     </LabelInputContainer>
 
                     <button
