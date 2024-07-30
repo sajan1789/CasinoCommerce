c641d48a968bbd7d8bb2eef7005e5a96151bdb32
sajan1789 <sajan99559@gmail.com>
2024-07-31 00:26:49 +0530

    yy

--- my-app/src/app/contact/page.jsx
+++ my-app/src/app/contact/page.jsx
@@ -19,6 +19,7 @@ function SignupFormDemo() {
         e.preventDefault();
         console.log("Form submitted");
     };
+     console.log(message,"dd")
     return (
         <div className="w-[75%] mx-auto rounded-none md:rounded-2xl md:p-8 shadow-input dark:bg-black  mt-[130px] flex  m-auto items-center justify-center ">
             <div className=" flex flex-col w-[50%] px-10 gap-10 z-10">
@@ -76,18 +77,17 @@ function SignupFormDemo() {
                     <LabelInputContainer className="mb-4">
                         <Label htmlFor="password">Message</Label>
                         <textarea
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
-                            placeholder="Your message"
-                            className="flex h-[100px] w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
-            file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-            focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
-             disabled:cursor-not-allowed disabled:opacity-50
-             dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
-             group-hover/input:shadow-none transition duration-400"
-                            
-                            required
-                        > </textarea>
+    value={message}
+    onChange={(e) => setMessage(e.target.value)}
+    placeholder="Your message"
+    className="flex h-[100px] w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
+        file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
+        focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+        disabled:cursor-not-allowed disabled:opacity-50
+        dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+        group-hover/input:shadow-none transition duration-400"
+    required
+/>
                     </LabelInputContainer>
 
                     <button
