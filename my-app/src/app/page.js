e857aa128494f48e06dd47ea380336740a8e64a2
sajan1789 <sajan99559@gmail.com>
2024-07-30 23:33:33 +0530

    bug-fixed

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -5,10 +5,11 @@ import { FeaturesSectionDemo } from "@/components/FeaturesSectionDemo";
 import Products from "@/components/Products";
 import { GameProvider } from "@/components/GameProvider";
 import { Review } from "@/components/Review";
-import { Example } from "@/components/example";
+// import { Example } from "@/components/example";
 import { WavyBackground } from "@/components/ui/wavy-background";
 import Developers from "@/components/Developers";
 import Footer from "@/components/Footer";
+import { Example } from "@/components/Example";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col  mt-4 justify-between antialiased">
